f584bc506a7464b0eaf902adcd13ba67052250df
Annabel <belladfui@gmail.com>
2023-11-02 18:48:52 -0600

    created app

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'planner_app',
 ]
 
+# Add support for authenticating users
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
